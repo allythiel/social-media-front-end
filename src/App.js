26d09221396f020d4ee2eddc825b5d5ebc2338b7
Craig Satchell <satchdev@gmail.com>
2021-07-22 19:26:24 -0400

    mod main app layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/header/header';
+import Footer from './components/footer/Footer';
+import Profile from './components/profile/Profile';
+import FriendsList from './Components/FriendsAllUsersList/friendsList/friendsList';
+import Timeline from './Components/timeline/timeline';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+   return (
+      <div className='App'>
+         <Header />
+         <div className="content">
+            <Profile />
+            <Timeline />
+            <FriendsList />
+         </div>
+         <Footer />
+      </div>
+   )
 }
 
-export default App;
+export default App
