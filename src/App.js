985c31aa560a963b4390df038b596580a06ea748
Craig Satchell <satchdev@gmail.com>
2021-07-22 20:43:05 -0400

    mod css wireframe

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react'
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Profile from './components/profile/Profile';
-import FriendsList from './components/FriendsAllUsersList/friendsList/FriendsList';
-import Timeline from './components/timeline/Timeline';
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Profile from './components/profile/Profile'
+import FriendsList from './components/FriendsAllUsersList/friendsList/FriendsList'
+import Timeline from './components/timeline/Timeline'
+import './App.css'
 
 const App = () => {
    return (
       <div className='App'>
-         <Header />
-         <div className="content">
-            <Profile />
-            <Timeline />
-            <FriendsList />
+         <Header/>
+         <div className='content'>
+            <div>
+               <Profile />
+            </div>
+            <div>
+               <Timeline/>
+            </div>
+            <div>
+               <FriendsList/>
+            </div>
          </div>
-         <Footer />
+         <Footer/>
       </div>
    )
 }
