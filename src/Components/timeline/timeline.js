50770bf9a5deed126696a9766aee19e03943b280
Craig Satchell <satchdev@gmail.com>
2021-07-23 05:33:18 -0400

    mod ui layout css

--- src/Components/timeline/timeline.js
+++ src/Components/timeline/timeline.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import UserPost from './userPost/UserPost';
 import './timeline.css';
 
 const Timeline = () => {
     return (
         <div className='timeline'>
-            <h3>Timeline</h3>
+          <h3>Timeline</h3>
+          <UserPost name='Bradley Cooper' />
+          <UserPost name='Sheila Thomas' />
+          <UserPost name='Tanya Makeover' />
+          <UserPost name='Sara Fisher' />
+
         </div>
     )
 }
--- src/Components/timeline/userPost/userPost.js
+++ src/Components/timeline/userPost/userPost.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import './userPost.css';
 
-const UserPost = () => {
-    return (
-        <div className='user-post'>
-            
-        </div>
-    )
+const UserPost = (props) => {
+   return (
+      <div className='user-post'>
+         <p>
+            <span>{`${props.name} >>`}&nbsp;&nbsp;</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolor ratione sed assumenda, eligendi quia harum, commodi molestiae dolores <i><b>~3 hours ago</b></i></p>
+      </div>
+   )
 }
 
 export default UserPost;
