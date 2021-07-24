0aaaf86b376a8c55c098a7b79afa876dea363456
Craig Satchell <satchdev@gmail.com>
2021-07-24 13:05:31 -0400

    mod FriendsList scrollbar behavior

--- src/_demoData/friends.js
+++ src/_demoData/friends.js
@@ -8,31 +8,37 @@ const myFriends = [
    {
       name: 'Bradley Beal',
       avatarURL: avatarURL
-   },   {
+   },
+   {
       name: 'Lebron James',
       avatarURL: avatarURL
-   },   {
+   },
+   {
       name: 'Breanna Stewart',
       avatarURL: avatarURL
-   },   {
+   }, {
       name: 'Tanya Makeover',
       avatarURL: avatarURL
-   },   {
+   }, {
       name: 'Steph Curry',
       avatarURL: avatarURL
-   },
-   {
+   }, {
       name: 'Sara Fisher',
       avatarURL: avatarURL
-   },
-   {
+   }, {
       name: 'Shela Thompson',
       avatarURL: avatarURL
-   },
-   {
+   }, {
       name: 'Bradley Cooper',
       avatarURL: avatarURL
+   }, {
+      name: 'Brian Jackson',
+      avatarURL: avatarURL
+   }, {
+      name: 'Lesle Thomas',
+      avatarURL: avatarURL
    },
+
 ]
 
 export default myFriends;
