f2343758e81aa8b0b9996459edb321bc2c2becf9
Craig Satchell <satchdev@gmail.com>
2021-07-27 02:25:48 -0400

    add MessageBar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Header from './components/header/header'
 import Main from './components/main/main';
 import AppLogin from './components/appLogin/appLogin';
+import MessageBar from './components/messageBar/messageBar';
 import axios from 'axios';
 import './App.css'
 
@@ -16,6 +17,8 @@ const App = () => {
    const [loggedIn, setLoggedIn] = useState(false);
    const [register, setRegister] = useState(false);
    const [registering, setRegistering] = useState(false);
+   const [messageText, setMessageText] = useState('');
+   const [showMessageBar, setShowMessageBar] = useState(true);
 
 
    const aboutme = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere eos illum accusantium. Animi, consequuntur assumenda!';
@@ -37,10 +40,11 @@ const App = () => {
    }, [])
 
 
-   
+
    return (
       <div id='app' className='App'>
          <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+         { showMessageBar && <MessageBar messageText={'This is a sample information message'} setShowMessageBar={setShowMessageBar} /> }
          <div className='content'>
             {(!loggedIn || registering) && <AppLogin register={register} registering={registering} setRegistering={setRegistering} setRegister={setRegister} setLoggedIn={setLoggedIn} />}
             {loggedIn && <Main aboutme={aboutme} name={name} friends={friends} />}
