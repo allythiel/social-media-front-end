c45c132bc5eee7e350b0374e5b703fa599b67202
Craig Satchell <satchdev@gmail.com>
2021-07-29 11:35:02 -0400

    create profile edit form in Profile component

--- src/Components/profile/Profile.js
+++ src/Components/profile/Profile.js
@@ -4,15 +4,23 @@ import UserInfo from './userInfo/userInfo';
 import './profile.css'
 
 const Profile = (props) => {
-   const [editProfile, setEditProfile] = useState(false);
+   const [editMode, setEditMode] = useState(false);
+
 
    return (
       <div className='profile'>
          <h3>Profile</h3>
+         {/* display profile */}
          <Avatar currentUser={props.currentUser} />
          <UserInfo currentUser={props.currentUser} />
-         {(!editProfile && (props.loggedInUser.email === props.currentUser.email)) && <button className="button-md" onClick={() => { setEditProfile(true) }}>Edit Profile</button>}
-         {(editProfile && (props.loggedInUser.email === props.currentUser.email)) &&<button className="button-md" onClick={() => { setEditProfile(false) }}>Save Changes</button>}
+         {(!editMode && (props.loggedInUser.email === props.currentUser.email)) && <button className="button-md" onClick={() => { setEditMode(true) }}>Edit Profile</button>}
+
+         {/* edit profile mode */}
+         {(editMode && (props.loggedInUser.email === props.currentUser.email)) && <form onSubmit={(event) => { event.preventDefault() }}>
+            <input type="text" name='name' placeholder='Your name' value={props.editProfile.name} /><br />
+            <input type="text" name='aboutme' placeholder='Tell us all about you' value={props.editProfile.aboutMe} />
+         </form>}
+         {(editMode && (props.loggedInUser.email === props.currentUser.email)) && <button className="button-md" onClick={() => { setEditMode(false) }}>Save Changes</button>}
       </div>
    )
 }
