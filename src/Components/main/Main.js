381e7a4db581f2bb7cf28f79ce5a5c9600513b1c
Craig Satchell <satchdev@gmail.com>
2021-07-29 12:50:08 -0400

    add edit capability to edit profile funct.

--- src/Components/main/Main.js
+++ src/Components/main/Main.js
@@ -10,7 +10,7 @@ const Main = (props) => {
       <div className='main'>
          <div>
             <div>
-               <Profile currentUser={props.currentUser} loggedInUser={props.loggedInUser} editProfile={props.editProfile} handleProfileChange={props.handleProfileChange} handleProfileSubmit={props.handleProfileSubmit} />
+               <Profile currentUser={props.currentUser} loggedInUser={props.loggedInUser} editProfile={props.editProfile} handleEditProfileChange={props.handleEditProfileChange} handleEditProfileSubmit={props.handleEditProfileSubmit} />
             </div>
             <div>
                <Timeline postings={props.postings} setPostings={props.setPostings} newPosting={props.newPosting} setNewPosting={props.setNewPosting} handleNewPostingChange={props.handleNewPostingChange} handleNewPostingSubmit={props.handleNewPostingSubmit} currentUser={props.currentUser} loggedInUser={props.loggedInUser} />
